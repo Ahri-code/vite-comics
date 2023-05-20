fa4e645765ea71b16aef7a12603e6934762b6812
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-20 17:12:08 +0200

    feat(vue): follow us part

--- src/components/SigninFooter.vue
+++ src/components/SigninFooter.vue
@@ -1,13 +1,27 @@
 <script>
 export default {
-    name: "SigninFooter"
+    name: "SigninFooter",
+    data() {
+        return {
+            links: ["../assets/footer-facebook.png", "../assets/footer-twitter.png", "../assets/footer-youtube.png", "../assets/footer-pinterest.png", "../assets/footer-periscope.png"]
+        }
+    },
+    methods: {
+        getImagePath: function (imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
 }
 </script>
 
 <template>
     <div id="container">
         <div id="contained">
-
+            <button>sign-up now!</button>
+            <div id="follow">
+                <h3>follow us</h3>
+                <a v-for="link in links" href="#"><img :src="getImagePath(`${link}`)"></a>
+            </div>
         </div>
     </div>
 </template>
@@ -31,5 +45,26 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+button {
+    border: 3px solid #0282f9;
+    padding: 1rem;
+    background-color: #303030;
+    color: white;
+    text-transform: uppercase;
+}
+
+#follow {
+    width: 35%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-transform: uppercase;
+    color: #0282f9;
+}
+
+h3 {
+    font-size: 20px;
 }
 </style>
